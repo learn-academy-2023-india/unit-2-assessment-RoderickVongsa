dd3c9acb943c6b9d0cc44197fab41b9b42a982c2
RoderickVongsa <roderick.vongsa@gmail.com>
2023-12-11 09:38:47 -0800

    struggled with challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -5,7 +5,7 @@
 // If you get stuck, leave comments to help us understand your thought process
 
 // Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
+// Add appropriate dependency
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
@@ -27,16 +27,39 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // a) Create a test with expect statements for each of the variables provided.
 
+
 const object1 = { number: 15 }
+
+
 // Expected output: "15 is divisible by three"
 const object2 = { number: 0 }
+// Using loop for this  problem
+// ran with node and it seemed to work as it gave me "15 is divisible by three"
+for(let i= 1; i <= object2.number; i++){
+    if (i === object2.number && i % 3 === 0) {
+        console.log(`${object2.number} is divisible by three`);
+    } else if (i % 3 === 0) {
+        console.log(i)
+    }
+}
 // Expected output: "0 is divisible by three"
 const object3 = { number: -7 }
+
+for(let i= 1; i <= object3.number; i++){
+    if (i === object3.number && i % 3 === 0) {
+        console.log(`${object3.number} is divisible by three`);
+    } else if (i % 3 === 0) {
+        console.log(i)
+    }
+}
+
 // Expected output: "-7 is not divisible by three"
 
 // b) Create the function that makes the test pass.
-
 // Pseudo code:
+// tried to run it but it seems to fail when running yarn jest
+// was not able to make the function pass
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
@@ -47,6 +70,25 @@ const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
+const capitalizeFirstLetter = (words) => {
+    return words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  };
+
+  test("Capitalize first letter of each word in randomNouns1", () => {
+    const result = capitalizeFirstLetter(randomNouns1);
+    const expected = ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"];
+    expect(result).toEqual(expected);
+  });
+  
+  test("Capitalize first letter of each word in randomNouns2", () => {
+    const result = capitalizeFirstLetter(randomNouns2);
+    const expected = ["Temperature", "Database", "Chopsticks", "Mango"];
+    expect(result).toEqual(expected);
+  });
+  
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// created a function called "capitalizeFirstLetter" and to return with .toUpperCase + .slice so I can separate the index from the 1st word
+// The test passed
+// Did the same with second randomNouns was also able to pass
